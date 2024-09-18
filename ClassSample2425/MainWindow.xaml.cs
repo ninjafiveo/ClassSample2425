63e6fcb3f55adfe341f874cdaf31d073ea0baff5
ninjafiveo <mjsekol@live.com>
2024-09-18 12:58:37 -0400

    Add project files.

---
+++ ClassSample2425/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassSample2425
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBig_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello WPF");
+            Big_Button.Background = new SolidColorBrush(Colors.Honeydew);
+
+        }
+
+        private void Big_Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Big_Button.Background = new SolidColorBrush(Colors.DeepPink);
+        }
+    }
+}
+
